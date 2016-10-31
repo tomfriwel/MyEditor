5f91921076ee23a183a7d88b02549c8a8799843b
tomfriwel <tomfriwel@tomdemacbook-pro.local>
2016-10-31 17:44:59 +0800

    Initial Commit

---
+++ MyEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyEditor
+//
+//  Created by tomfriwel on 31/10/2016.
+//  Copyright © 2016 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyEditor
+//
+//  Created by tomfriwel on 31/10/2016.
+//  Copyright © 2016 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
